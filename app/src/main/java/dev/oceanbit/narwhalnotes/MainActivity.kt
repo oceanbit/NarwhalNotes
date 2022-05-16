89dd68ee9f0c89338c0c558570ed532124b118eb
Corbin Crutchley <git@crutchcorn.dev>
2022-05-15 17:22:46 -0700

    chore: introduce lazy column to map out messages

--- app/src/main/java/dev/oceanbit/narwhalnotes/MainActivity.kt
+++ app/src/main/java/dev/oceanbit/narwhalnotes/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.items
 import dev.oceanbit.narwhalnotes.ui.theme.NarwhalNotesTheme
 import java.text.SimpleDateFormat
 import java.util.*
@@ -39,19 +41,23 @@ fun Message(message: String, sentTime: Date) {
     }
 }
 
+data class MessageData(val message: String, val date: Date);
+
 @Composable
 fun MessageScreen() {
+    val messages = (0..16).map { i ->
+        MessageData("Test", Date())
+    }
+
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        Column(Modifier.padding(16.dp), verticalArrangement = Arrangement.spacedBy(16.dp)) {
-            Message("Testing", Date())
-            Message("Testing", Date())
-            Message("Testing", Date())
-            Message("Testing", Date())
-            Message("Testing", Date())
-            Message("Testing", Date())
+        LazyColumn(Modifier.padding(16.dp), verticalArrangement = Arrangement.spacedBy(16.dp)) {
+            items(messages) { message ->
+                Message("Testing", Date())
+            }
         }
     }
 }
